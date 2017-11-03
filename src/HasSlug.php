4057307f0c7fa43ff105483e822456287ca5700a
Franco Gilio <franco.gilio@gmail.com>
2017-11-02 21:05:05 -0300

    Make `generateSlug` static

--- src/HasSlug.php
+++ src/HasSlug.php
@@ -9,11 +9,11 @@ trait HasSlug
     public static function bootHasSlug()
     {
         static::saving(function (Model $model) {
-            $model->slug = $model->generateSlug($model->name);
+            $model->slug = static::generateSlug($model->name);
         });
     }
 
-    protected function generateSlug($name): string
+    protected static function generateSlug($name): string
     {
         $slugger = config('tags.slugger');
 
